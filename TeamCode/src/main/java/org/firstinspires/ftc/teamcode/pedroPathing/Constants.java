9ac4595077cf9613ecd7998ca79db29545364267
tadashicarnelos <progsftc@gmail.com>
2026-09-05 09:00:44 -0300

    Configure Pedro Follower with Mecanum and Pinpoint

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,19 +1,27 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.pedropathing.follower.Follower;
-import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+/**
+ * Central Pedro Pathing configuration and Follower factory.
+ */
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    // These are intentionally conservative until the robot is tuned.
+    public static PathConstraints pathConstraints =
+            new PathConstraints(0.99, 100, 1, 1);
 
+    /**
+     * Creates the Pedro Follower with our Mecanum drivetrain and Pinpoint localizer.
+     */
     public static Follower createFollower(HardwareMap hardwareMap) {
-        return new FollowerBuilder(followerConstants, hardwareMap)
+        return new FollowerBuilder(FConstants.followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(FConstants.driveConstants)
+                .pinpointLocalizer(LConstants.localizerConstants)
                 .build();
     }
 }
